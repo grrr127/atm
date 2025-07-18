4a762e289bf99076c6da2506e87865610c68d9ed
grrr127 <qrrr127@gmail.com>
2025-07-18 13:25:05 +0900

    ✨ 영수증 보기 기능 추가
    영수증 내역을 확인할 수 있는 기능 추가 및 영수증 내역 없을 시 안내 메시지 작성

--- atm.py
+++ atm.py
@@ -2,6 +2,7 @@
 # 사용자로부터 atm 기계에 사용 목적에 맞는 기능을 선택할 수 있도록 (입금, 출금, 종료, 입출금 내역 영수증)
 
 balance = 500000
+receipts = []
 
 while True : 
     num = input("사용하실 기능을 선택해주세요 (1.입금, 2.출금, 3.영수증 보기, 4.종료) : ")
@@ -13,14 +14,21 @@ while True :
         deposit_amount = int(input('입금할 금액을 입력해주세요: '))
         balance += deposit_amount
         print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        receipts.append(('입금', deposit_amount, balance))
 
     if num == '2':
         withdraw_amount = int(input('출금할 금액을 입력해주세요: '))
         withdraw_amount = min(balance, withdraw_amount)
         balance -= withdraw_amount
-        print(f'출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')        
+        print(f'출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        receipts.append(('출금', withdraw_amount, balance))        
     
     if num == '3':
-        pass
+        if receipts:
+            for i in receipts:
+                print(f'{i[0]}: {i[1]}원 | 잔액: {i[2]}원')
+        
+        else:
+            print('영수증 내역이 없습니다.')
 
 print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.')
